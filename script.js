a439b67483361cf51ae38ed8b531017d11481afc
Matheus Gomes <matheusoutros@hotmail.com>
2023-01-23 23:13:55 -0300

    Colocando todo JS da aula 4 da RocketSeat

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const form = document.querySelector('#forMother');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  water: ['01-01', '01-02', '01-03'],
-  food: ['01-01', '01-02', '01-03']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert("Dia já incluso!")
+    return
+  }
+
+  alert("Adicionado com sucesso!")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("FeitoComNLW", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("FeitoComNLW")) || {}
 nlwSetup.setData(data)
 nlwSetup.load(data)
-
-const mensage = Abce;
-alert(mensage)
